49bccdea67621da04388fb3e189042bcd754f076
valera-rozuvan <valera.rozuvan@gmail.com>
2012-12-09 10:32:41 +0200

    Modified the menu. Several improvements to code.

--- js_author/experiments_menu.js
+++ js_author/experiments_menu.js
@@ -1,4 +1,7 @@
-define(['text!toc.json', 'jquery', 'ModuleDiv', 'Output', 'RunModules'], function (Toc, $, ModuleDiv, Output, RunModules) {
+define(
+    ['text!toc.json', 'jquery', 'ModuleDiv', 'Output', 'RunModules'],
+    function (Toc, $, ModuleDiv, Output, RunModules) {
+
     var moduleDiv, p, out;
 
     moduleDiv = ModuleDiv('Experiments menu. A list of all available JS experiments.');
@@ -7,82 +10,109 @@ define(['text!toc.json', 'jquery', 'ModuleDiv', 'Output', 'RunModules'], functio
     return ExperimentsMenu;
 
     function ExperimentsMenu() {
-        var toc, c1, todaysDate;
+        var toc, c1, todaysDate, day, month, year;
 
         moduleDiv.addCaption();
 
-        toc = JSON.parse(Toc);
+        out(
+            '<div style="text-align: right;">' +
+                'Valera Rozuvan |' +
+                ' <a href="https://github.com/valera-rozuvan/javascript-experiments">' +
+                    'source on GitHub' +
+                '</a>' +
+            '</div>'
+        );
 
-        out('<div style="text-align: right;">' +
-                'Author: Valera Rozuvan | Source: <a href="https://github.com/valera-rozuvan/javascript-experiments">on GitHub</a>' +
-            '</div>');
+        toc = JSON.parse(Toc);
 
         out('<ul>');
         for (c1 = 0; c1 < toc.length; c1 += 1) {
-            out('<li data-module_index="' + c1 + '">' + toc[c1].moduleName  + '</li>');
+            out(
+                '<li data-module_index="' + c1 + '">' +
+                    toc[c1].moduleName  +
+                '</li>'
+            );
         }
         out('</ul>');
 
+        todaysDate = new Date();
 
-        (function () {
-            var today = new Date();
-            var dd = today.getDate();
-            var mm = today.getMonth()+1; //January is 0!
+        day = todaysDate.getDate();
+        month = todaysDate.getMonth() + 1; // January is 0.
+        year = todaysDate.getFullYear();
 
-            var yyyy = today.getFullYear();
-            if(dd<10) {
-                dd='0'+dd;
-            }
-            if(mm<10) {
-                mm='0'+mm;
-            }
+        if (day < 10) {
+            day = '0' + day;
+        }
+        if (month < 10) {
+            month = '0' + month;
+        }
 
-            todaysDate = mm+'/'+dd+'/'+yyyy;
-        }());
+        out(
+            '<div style="display: block; clear: both; text-align: right;">' +
+                day + '.' + month + '.' + year +
+            '</div>'
+        );
 
-        out('<div style="display: block; clear: both; text-align: right;">' +
-                todaysDate +
-            '</div>');
-
+        // Whenever an item in the menu is clicked, we will hide what is
+        // already showing, and then show the content for the clicked item.
         moduleDiv.children('li').each(function (index, value) {
             $(value).click(function () {
-                var moduleIndex, numDivsLeft;
-
-                moduleIndex = $(value).data('module_index');
-
-                // $('.page').empty();
-
-                // Instead of a simple hide(), let us do something a bit more
-                // fancy.
-
-                numDivsLeft = $('.page').children('.module').length;
-
-                if (numDivsLeft === 0) {
-                    showSelectedExperiment();
-                } else {
-                    $('.page').children('.module').each(function (index, value) {
-                        $(value).slideUp(500, function () {
-                            numDivsLeft -= 1;
-
-                            if (numDivsLeft === 0) {
-                                showSelectedExperiment();
-                            }
-                        });
-                    });
-                }
-
-                return;
-
-                function showSelectedExperiment() {
-                    $('.page').empty();
-
-                    $(document).attr('title', 'JavaScript Experiments: ' + toc[moduleIndex].moduleName);
-
-                    RunModules('experiments/' + toc[moduleIndex].moduleFolder, toc[moduleIndex].modulesToRun);
-                }
+                return menuItemClickHandler($(value).data('module_index'));
             });
         });
 
         moduleDiv.appendToSelector('.toc');
+
+        return;
+
+        function menuItemClickHandler(moduleIndex) {
+            var numDivsLeft, divsToProcess;
+
+            // $('.page').empty();
+
+            // Instead of a simple empty(), let us do something a bit more
+            // fancy. We will get all of the DIVs that must be removed, and
+            // we will hide them using the jQuery's slide up event. The
+            // last DIV to slide up will call the function to show the next
+            // set of content DIVs. If there are no DIVs to slide up, then
+            // we simply call the function to show the next set of content
+            // DIVs.
+
+            divsToProcess = $('.page').children('.module');
+
+            if (divsToProcess.length === 0) {
+                showSelectedExperiment();
+            } else {
+                numDivsLeft = divsToProcess.length;
+
+                divsToProcess.each(function (index, value) {
+                    $(value).slideUp(500, function () {
+                        numDivsLeft -= 1;
+
+                        if (numDivsLeft === 0) {
+                            showSelectedExperiment();
+                        }
+                    });
+                });
+            }
+
+            return;
+
+            // Function to show the next set of content DIVs.
+            function showSelectedExperiment() {
+                $('.page').empty();
+
+                $(document).attr(
+                    'title',
+                    'JavaScript Experiments: ' + toc[moduleIndex].moduleName
+                );
+
+                RunModules(
+                    'experiments/' + toc[moduleIndex].moduleFolder,
+                    toc[moduleIndex].modulesToRun
+                );
+            }
+        }
     }
 });
